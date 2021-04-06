058fc01c38f07e9a1f8c938e8619ec9f7b759863
Guido Stein <guido@liveby.com>
2021-04-05 22:08:06 -0400

    👌 IMPROVE: setup code for cron

--- fetch.js
+++ fetch.js
@@ -1,43 +1,50 @@
-const fetch = require('node-fetch')
+const SimplyRets = require('@liveby/simply-rets')
+const debug = require('debug')('simply-rets-mls-cache')
+
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 
-;(async () => {
-  const res = await fetch('https://api-v3.mbta.com/vehicles?filter%5Broute%5D=66', { headers: { accept: 'application/vnd.api+json' } }).then((r) => r.json())
-  const data = res.data.map(d => d.attributes)
+async function putNDJSON (ndjson) {
   const s3 = new S3Client({ region: 'us-east-2' })
   const d = new Date()
-  const objectName =
-    d.toISOString().split('T')[0].split('-').slice(0, 2).join('/') +
-    '/' +
-    d.toISOString()
+  const objectName = d.toISOString().split('T')[0].split('-').slice(0, 2).join('/') + '/' + d.toISOString()
 
   const uploadParams = {
     Bucket: 'liveby--guido-data',
-    Key: 'objectName',
-    Body: data
+    Key: objectName,
+    Body: ndjson
   }
 
+  const req = new PutObjectCommand(uploadParams)
   try {
-    const data = await s3.send(new PutObjectCommand(uploadParams))
+    const data = await s3.send(req)
     console.log('Success', data.Buckets)
   } catch (err) {
     console.log('Error', err)
   }
-})()
+}
 
-async function updateCache () {
-  const db = await connect()
+async function getData () {
   const apps = [{ user: 'admin_3542b037', password: '85l12u5322623p17' }]
   // const apps = await db.db('LiveBy').collection('simply-rets').find().toArray()
-  for (const app of apps) {
-    try {
-      debug('running for: ', app.user)
-      await updateCacheForApp(new SimplyRets(app))
-      await sleep(1000)
-    } catch (e) {
-      logger.error(e.stack)
-    }
+
+  const app = apps[0]
+  try {
+    debug('running for: ', app.user)
+    const simplyRets = await new SimplyRets(app)
+    const { vendors } = await simplyRets.options()
+
+    const sr = await simplyRets.properties({ vendor: vendors[0] })
+
+    let ndjson = ''
+    sr.forEach(element => {
+      ndjson = ndjson.concat('\n', JSON.stringify(element))
+    })
+    return ndjson
+  } catch (e) {
+    console.error(e)
+    throw e
   }
-  debug('updated all properties...')
-  await updateCache()
 }
+
+getData()
+  .then(putNDJSON)
